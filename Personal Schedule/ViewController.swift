0ef5cf62b2150ea6d5706304712f5ea0c7289623
Frederic Orlando <frederic.orlando28@gmail.com>
2020-08-19 11:46:21 +0700

    Initial Commit

---
+++ Personal Schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personal Schedule
+//
+//  Created by Frederic Orlando on 19/08/20.
+//  Copyright © 2020 Frederic Orlando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Personal ScheduleTests/Personal_ScheduleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Personal_ScheduleTests.swift
+//  Personal ScheduleTests
+//
+//  Created by Frederic Orlando on 19/08/20.
+//  Copyright © 2020 Frederic Orlando. All rights reserved.
+//
+
+import XCTest
+@testable import Personal_Schedule
+
+class Personal_ScheduleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
